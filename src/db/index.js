2e93712904799d8ae7c807a998a614cc20350b34
DakshDagar01 <dakshdagar884@gmail.com>
2025-07-08 18:44:47 +0530

    Update DB function

--- src/db/index.js
+++ src/db/index.js
@@ -3,8 +3,8 @@ import {DB_NAME} from '../constants.js';
 
 const DBconnect=async ()=>{
     try {
-       const connection=await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`) 
-       console.log(`\n Mongo DB connected successfully !!, ${connection.Connection}`)
+       const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`) 
+       console.log(`\n Mongo DB connected successfully !!, ${connectionInstance.connection.host}`)
     } 
     catch (error) {
         console.error('Error connecting to MongoDB:', error);
